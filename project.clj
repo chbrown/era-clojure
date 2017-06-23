1bfa5e50dc5af517bc1011fa657aae6d2679c211
Christopher Brown <io@henrian.com>
2017-06-23 13:58:57 -0500

    Version 0.2.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject era "0.2.0"
+(defproject era "0.2.1-SNAPSHOT"
   :description "Cross-platform time tools"
   :url "https://github.com/chbrown/era-clojure"
   :license {:name "Eclipse Public License"
